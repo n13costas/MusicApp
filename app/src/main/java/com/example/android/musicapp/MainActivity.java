0a74553f2730cac582a4c096e07c42add6514dca
Konstantinos Tsekos <konstantinos.tsekos@gmail.com>
2018-04-21 13:13:42 +0300

    Added list activity

--- app/src/main/java/com/example/android/musicapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.musicapp;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView startButton = findViewById(R.id.textStart);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent listIntent = new Intent(MainActivity.this, MusicListActivity.class);
+                startActivity(listIntent);
+            }
+        });
+
     }
 }
+
